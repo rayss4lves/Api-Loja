8452531bc0ea25a69e227273239472b9e725b83f
rayss4lves <rayssasantos.com890@gmail.com>
2025-07-19 23:19:26 -0300

    primeiro commit do projeto api-loja

---
+++ src/main/java/dio/api_loja/ApiLojaApplication.java
@@ -0,0 +1,13 @@
+package dio.api_loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/api_loja/ApiLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.api_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
